c337f9fc6450f0cc28677d621917feff5e3c6417
Benjamin Baum <bjb253@cornell.edu>
2016-02-17 21:59:51 -0600

    (feat) create express skeleton and initial routes

---
+++ server/routes.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+var public = path.resolve('public') + '/';
+
+var routes = [
+  {
+    path: '/',
+    'get': function(req, res) {
+      res.sendFile(public + 'client/index.html');
+    }
+  },
+  {
+    path: '*',
+    get: function (req, res) {
+      res.redirect('/');
+    }
+  }  
+];
+
+module.exports = function (app) {
+  routes.forEach(function (route){
+    for (var key in route) {
+      if (key === 'path') { continue; }
+      app[key](route.path, route[key]);
+    }
+  });
+};
---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./routes');
+
+var app = express();
+
+app.use(morgan('tiny'));
+app.use(bodyParser());
+
+var port = process.env.PORT || 8080;
+
+
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+routes(app);
+
+
+
+
+
+
