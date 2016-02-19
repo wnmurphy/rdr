5697364525560c4e9eac1b830b57e42bafe6df92
Benjamin Baum <bjb253@cornell.edu>
2016-02-18 23:18:46 -0600

    (feat) implement findOrCreate helper and associated specs

--- server/models/helpers.js
+++ server/models/helpers.js
@@ -1,13 +1,33 @@
 var models = require('./models.js');
+var Promise = require('bluebird');
 
 var findOrCreate = function (Model, attributes) {
 
-  Model.forge(attributes).fetch()
-  .then(function (found) {
+  return new Promise (function (resolve, reject) {
 
-  })
-  .catch(function (err) {
+    Model.forge(attributes).fetch()
+    .then(function (model) {
+      if (!model) {
+        model = new Model(attributes);
+      }
+      model.save()
+      .then(function () {
+        resolve(model);
+      })
+      .catch(function (error) {
+        reject(error);
+      })
+    })
+    .catch(function (error) {
+      reject(error);
+    });
 
   });
 
 };
+
+module.exports = {
+
+  findOrCreate: findOrCreate
+
+}
