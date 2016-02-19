8981707e96de803029a80dad9ccf26e50293f753
Benjamin Baum <bjb253@cornell.edu>
2016-02-18 23:18:46 -0600

    (feat) implement findOrCreate helper and associated specs

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,6 +1,9 @@
+var path = require('path');
 var expect = require('chai').expect;
-var Author = require(__dirname + '/../server/models/book.js').Author;
-var Book = require(__dirname + '/../server/models/book.js').Book;
+var db = require(path.resolve('db/db'));
+var Author = require(__dirname + '/../server/models/models.js').Author;
+var Book = require(__dirname + '/../server/models/models.js').Book;
+var helpers = require(path.resolve('server/models/helpers'));
 
 
 //
@@ -10,7 +13,7 @@ describe('', function() {
     
   });
 
-  describe('Bookshelf models: ', function() {
+  describe('Bookshelf ', function() {
 
     it('Should retrieve author from book', function(done) {
       Author.forge({name: 'Dr. Seuss'}).fetch()
@@ -36,6 +39,49 @@ describe('', function() {
             });
           });
         });
+    });
+
+    describe('Find or create', function () {
+      it('Should provide an object', function(done) {
+        helpers.findOrCreate(Author, {name: 'Chicken Salad'})
+        .then(function (newAuthor) {
+          expect(newAuthor).to.be.an('object');
+          done();
+        });
+      });
+
+      it('Should retrieve the existing model when one exists', function(done) {
+        Author.forge({}).fetch()
+        .then(function (author) {
+          var id = author.get('id');
+          helpers.findOrCreate(Author, author.attributes)
+          .then(function (retrieved) {
+            expect(retrieved.get('id')).to.equal(id);
+            done();
+          });
+        });
+      });
+
+      it('Should create a new model when there are no matches', function(done) {
+        db.knex('authors').max('name').then(function (name) {
+            var checkName;
+            for (var key in name[0]) {
+              checkName = name[0][key];
+            }
+            db.knex('authors').max('id').then(function (id) {
+            helpers.findOrCreate(Author, {name: checkName + 'z'})
+            .then(function (retrieved) {
+              var checkID;
+              for (var key in id[0]) {
+                checkID = id[0][key];
+              }
+              expect(retrieved.get('id')).to.be.above(checkID);
+              done();     
+              retrieved.destroy();   
+            });
+          });
+        });
+      });
     });
   }); 
 });
