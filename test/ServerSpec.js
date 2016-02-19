ea4335cd56a100e533c0486a800f7f7fd921dcc0
Josh Riesenbach <josh.riesenbch@gmail.com>
2016-02-18 17:36:07 -0700

    Adds files not included in last commit

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,4 +1,6 @@
 var expect = require('chai').expect;
+var Author = require(__dirname + '/../server/models/book.js').Author;
+var Book = require(__dirname + '/../server/models/book.js').Book;
 
 
 //
@@ -8,10 +10,27 @@ describe('', function() {
     
   });
 
-  describe('Something: ', function() {
+  describe('Bookshelf models: ', function() {
 
-    it('should do something', function() {
-      expect(true).to.equal(true);
+    it('Should retrieve author from book', function(done) {
+      var author = new Author({
+        name: 'Dr. Seuss'
+      });
+      author.save().then( function () {
+        var book = new Book({
+          title: 'Cat in the Hat',
+          author_id: author.get('id')
+        });
+
+        book.save().then(function () {
+
+          Book.forge({title: 'Cat in the Hat'}).fetch({withRelated: ['author']})
+          .then(function (book) {
+            expect(book.related('author')).to.deep.equal(author);
+            done();
+          });
+        });
+      });
     });
 
   }); 
