093aa19e52118124ae08b06abad45638668dba85
Benjamin Baum <bjb253@cornell.edu>
2016-02-19 10:42:13 -0600

    (feat) allow server to accept post requests to /users/books. If valid author/book information is provided, will send a 201. Otherwise, 409.

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
 var db = require('../db/db.js');
 var path = require('path');
 var public = path.resolve('public') + '/';
+var helpers = require(path.resolve('server/models/helpers'));
+var Author = require(path.resolve('server/models/models')).Author;
+var Book = require(path.resolve('server/models/models')).Book;
 
 var routes = [
   {
@@ -14,8 +17,40 @@ var routes = [
     get: function (req, res) {
       res.redirect('/');
     }
+  },
+  {
+    path: '/users/books',
+    //TODO: take reactions into account
+    'post': function (req, res) {
+      var author = req.body.author;
+      var book = req.body.book; 
+      if (!author || !book) {
+        res.sendStatus(409);
+      } else {
+
+        helpers.findOrCreate(Author, author)
+        .then(function (author) {
+          book.author_id = author.get('id');
+          helpers.findOrCreate(Book, book)
+          .then(function (book) {
+          //posted a valid book
+            res.statusCode = 201;
+            res.end();
+          })
+          .catch(function (error) {
+            console.error(error);
+            res.sendStatus(409);
+          });
+        })
+        .catch(function (error) {
+          console.error(error);
+          res.sendStatus(409);
+        });  
+      }    
+    }
   }  
 ];
+
 
 module.exports = function (app, express) {
   app.use(express.static(public + '/client'))
