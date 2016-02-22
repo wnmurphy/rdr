35b3f7cce53bfa3a81718747941e4ad7e0f86ac5
Benjamin Baum <bjb253@cornell.edu>
2016-02-22 16:00:07 -0600

    (feat) allow for posting of a book and displaying on the page

--- public/client/app/shared/user.page.controller.js
+++ public/client/app/shared/user.page.controller.js
@@ -2,7 +2,7 @@ angular.module('booklist.user', [])
 
 .controller('UserController', ['$scope', 'Books','$rootScope', function($scope, Books, $rootScope){
   $scope.user = {};
-  // $scope.books = $rootScope.books
+  $scope.books = [];
 
   $scope.bookTemplate = 'app/shared/book.entry.html';
 
@@ -13,15 +13,25 @@ angular.module('booklist.user', [])
   $scope.setReaction = function ($event, reaction) {
     var $target = $($event.currentTarget);
     $('.reactions').find('.selected').removeClass('selected');
-    $target.addClass('selected');
+    if ($scope.reaction === reaction) {
+      $scope.reaction = 0;
+    } else {
+      $target.addClass('selected');
+      $scope.reaction = reaction;
+    }
   };
 
   $scope.addBook = function(){
 
-    console.log($scope.reaction);
+
     //TODO: check how to do error handling
     Books.postBook($scope.bookTitle, $scope.authorName, $scope.reaction)
     .then(function(resp){
+      if (resp.book && resp.author) {
+        var book = resp.book;
+        book.authorName = resp.author.name;
+        $scope.books.push(book);
+      }
     })
     .catch(function(error){
       console.log(error);
