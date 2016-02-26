6e214e1ec902d035cda978f823e922f2c80bb8ba
Benjamin Baum <bjb253@cornell.edu>
2016-02-26 15:19:02 -0600

    (chore) create tasks for uglification, minification, concatenation. create a task that watches all client files and runs these tasks upon save

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,60 @@ module.exports = function(grunt) {
         tasks: [
           'sass'
         ]
+      },
+      dev: {
+        files: [
+          'public/client/**/*.js',
+          'public/client/**/*.html',
+          'public/client/**/*.scss',
+          '!public/client/dist/**/*',
+          '!.min.css',
+          '!.min.js'
+        ],
+        tasks: [
+          'build'
+        ]
+      }
+    },
+
+    cssmin: {
+      options: {
+        sourceMap: true,
+        target: 'public/client/dist'
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'public/client/assets/css/',
+            src: ['*.css', '!.min.css'],
+            dest: 'public/client/dist',
+            ext: '.min.css'
+          }
+        ]
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      app: {
+        src: [
+          'public/client/app/**/*.js'
+        ],
+        dest: 'public/client/dist/app.js'
+      }
+    },
+
+    uglify: {
+      scripts: {
+        files: {
+          'public/client/dist/app.min.js': ['public/client/dist/app.js']
+        },
+        options: {
+          sourceMap: "true"
+        }
       }
     }
 
@@ -64,17 +118,21 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
 
   grunt.registerTask('sassy', ['sass']);
 
-  grunt.registerTask('build', ['jshint', 'sass']);
+  grunt.registerTask('build', ['sass', 'cssmin', 'concat', 'uglify']);
   grunt.registerTask('test', ['mochaTest']);
 
   grunt.registerTask('default', ['watch']);
 
   grunt.registerTask('heroku:production', [
-
+    'build'
   ]);
+
 
 };
